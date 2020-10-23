08b9cf904b91819c359a56a8c4058308aa2d1dc5
Ezzkings <enzzooliveira0808@gmail.com>
2020-10-23 16:34:31 -0300

    extra changes
    changes on index, css and include js on index

--- js/script-calculator.js
+++ js/script-calculator.js
@@ -1,9 +1,62 @@
-function AddNumber(){
-    var TelaNum = window.document.getElementById('telaNum')
+function funcao(numbe){
+    document.getElementById('num').value+=numbe 
+}
 
+function simbols(){
+    
+}
+function equals(){
+
+}
+
+/*function addnum(){
+    document.getElementById("num").value='1'
+}
+function addnum2(){
+    document.getElementById("num").value='2'
+}
+function addnum3(){
+    document.getElementById("num").value='3'
+}*/
+
+
+/*var num = window.document.getElementById('num'.value)
+//let calc = "";
+
+function funcao(numbe){
+    //calc.push(param)
+    //putnum.innerHTML=(param)
+    calculo+=param;
+}
+
+/*=================================
+
+function addnum(){
+    var inputnum = window.document.getElementById('num'.value)
+    
+    var numbe1 = parseFloat(document.getElementById('num1').value)
+    var n1 =Number.parseFloat(document.getElementById(numbe1).value)
+    
+    //document.getElementById("num").value='1'
+}
+
+=================================*/
+
+/*
+    var numbe=parseFloat(document.getElementById('num').value)
+    numbe.innerHTML=1
+    /*
+    var number =parseFloat(document.getElementById('num').value)
+    var num1 =Number.parseFloat(number.value)
+    num1.value=1
+
+
+    //
+    //document.getElementById('num').value=0
+
+    /*var txt = window.document.getElementById('txt')
     var num0 = window.document.getElementById('num0')
     var n0 = Number.parseFloat(num0.value)
-
-    if(num0.selected)
-    document.getElementById('telaNum').value=0
-}
+    
+   if(n0.selected)
+    txt.inn=num1=======*/
